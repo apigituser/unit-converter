e591d705e0b82d63c6fb8dce75174aebae49e80e
apigituser <responsemail850@gmail.com>
2025-05-09 21:10:33 +0530

    organized templates

--- main.py
+++ main.py
@@ -9,7 +9,3 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
     return templates.TemplateResponse(request, "index.html")
-
-@app.post("/submit")
-def length(request: Request, length: float = Form(), length_from: str = Form(), length_to: str = Form()):
-    return {"Length": length, "From_length": length_from, "To_length": length_to}
